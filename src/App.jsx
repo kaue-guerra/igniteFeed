a86e27818c638a4becb26e6f3e69c1891104d0fc
Kaue Guerra <kaueguerra@gmail.com>
2022-08-01 22:20:46 -0300

    created sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { Header } from './components/Header';
-import './global.css';
+import { Header } from './components/Header'
+
+import './global.css'
+
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar'
 
 function App() {
-
   return (
-    <Header />
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main></main>
+      </div>
+    </>
   )
 }
 
